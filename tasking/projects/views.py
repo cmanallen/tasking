694e9147e421ebe226235dc6701f4c12da2a6828
cmanallen <cmanallen90@gmail.com>
2014-08-08 12:48:43 -0500

    fixed bug in updateproject view

--- tasking/projects/views.py
+++ tasking/projects/views.py
@@ -38,7 +38,7 @@ class UpdateProject(UpdateView):
 	template_name = 'manage_project.html'
 
 	def get_context_data(self, **kwargs):
-		context = super(UpdateProject, self).get_context_data(*kwargs)
+		context = super(UpdateProject, self).get_context_data(**kwargs)
 		context['action'] = reverse('update-project', kwargs={'pk': self.get_object().id})
 		return context
 

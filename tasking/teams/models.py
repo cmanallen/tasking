0f527912f6f2c6d7e59748c05c21d60037b21779
cmanallen <cmanallen90@gmail.com>
2014-08-19 11:43:07 -0500

    added team crud, added user count to team list

--- tasking/teams/models.py
+++ tasking/teams/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.db.models import Count
 from django.core.urlresolvers import reverse
 from utils.models import TimeStamp
 
 from django.conf import settings
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+# Model Managers
+class TeamManager(models.Manager):
+	def get_query_set(self):
+		return super(TeamManager, self).get_query_set().annotate(users=Count('user_team'))
 
 # Create your models here.
 class Team(TimeStamp):
@@ -13,6 +19,9 @@ class Team(TimeStamp):
 
 	# Relations
 	user_team = models.ManyToManyField(AUTH_USER_MODEL)
+
+	# Manager
+	objects = TeamManager()
 
 	# Model Methods
 	def get_absolute_url(self):

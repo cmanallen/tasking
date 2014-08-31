97acc99df1c86f8256c5f0fe9e1bb6619b1a6add
Colton Allen <cmanallen90@gmail.com>
2014-08-31 11:52:48 -0400

    Working towards implementing login required middleware

---
+++ tasking/tasking/middleware.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.contrib.auth.views import login
+from django.http import HttpResponseRedirect
+
+class LoginRequiredMiddleware(object):
+  def __init__(self):
+    self.require_login_path = getattr(settings, 'REQUIRE_LOGIN_PATH', '/users/login')
+
+  def process_request(self, request):
+    # if request.path != self.require_login_path and request.user.is_anonymous():
+    #   if request.POST:
+    #     return login(request)
+    #   else:
+    #     return HttpResponseRedirect('%s?next=%s' % (self.require_login_path, request.path))
+    if request.user.is_anonymous():
+      return HttpResponseRedirect('/users/login')
